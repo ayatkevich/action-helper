e3757bed58c49c1f6622b712627476e752d324e8
Alexander Yatkevich <ayatkevich@gmail.com>
2016-11-06 22:34:48 +0900

    feat: implement action creator

---
+++ src/index.js
@@ -0,0 +1,21 @@
+module.exports = function (type) {
+  if (!type) {
+    throw new TypeError('action function requires at least one argument');
+  }
+
+  var keys = [].slice.call(arguments, 1);
+
+  action.type = type;
+  return action;
+
+  function action(values) {
+    values = typeof values === 'object' ? values : {};
+    var result = {type: type};
+
+    keys.forEach(function (key) {
+      result[key] = values[key];
+    });
+
+    return result;
+  }
+};
---
+++ src/index.spec.js
@@ -0,0 +1,61 @@
+var test = require('ava');
+
+var actionFromPackage = require('../');
+var action = require('./');
+
+test('exports the same', function (t) {
+  t.is(action, actionFromPackage);
+});
+
+test('just type', function (t) {
+  t.deepEqual(action('TEST')(), {type: 'TEST'});
+});
+
+test('no type, no args', function (t) {
+  t.throws(function () {
+    action();
+  }, function (err) {
+    return err.name === 'TypeError';
+  });
+});
+
+test('can get a type without instantiation', function (t) {
+  t.is(action('TYPE').type, 'TYPE');
+});
+
+var authAction = action('LOGIN', 'username', 'password');
+
+test('can instantiate with args', function (t) {
+  t.deepEqual(authAction(), {
+    type: 'LOGIN',
+    username: undefined,
+    password: undefined
+  });
+});
+
+test('can instantiate with args and values passed as object', function (t) {
+  t.deepEqual(authAction({
+    username: 'User!',
+    password: '1234567'
+  }), {
+    type: authAction.type,
+    username: 'User!',
+    password: '1234567'
+  });
+});
+
+test('pass inappropriate value', function (t) {
+  t.deepEqual(authAction(function () {}), {
+    type: 'LOGIN',
+    username: undefined,
+    password: undefined
+  });
+});
+
+test('pass an array', function (t) {
+  t.deepEqual(action('HEAD_OF', '0', 'length')([1, 2, 3]), {
+    type: 'HEAD_OF',
+    0: 1,
+    length: 3
+  });
+});
